a61532b31667ad0ce6f88fecf322d05d31fbc022
inaimathi <inaimathi@self>
2014-03-11 16:49:07 -0400

    -Implemented repeatable/one-time technologies -Still working on upkeepable resources

--- Grg.elm
+++ Grg.elm
@@ -3,8 +3,6 @@ module Grg where
 import Graphics.Input as In
 import Graphics.Collage as Col
 import Dict as D
-import Maybe as M
-import Set as S
 
 -- Basic utility
 updateWithDefault : a -> comparable -> (a -> a) -> D.Dict comparable a -> D.Dict comparable a
@@ -19,42 +17,44 @@ decDict d table = let dec d (name, amt) = updateWithDefault 0 name (\n -> n - am
 find0 key dict = D.findWithDefault 0 key dict
 
 -- The Model
+data Reps = Count Int | One | Inf
 type Price = [(String, Int)]
 data Upgrade = Income String Int | Capacity String Int
 data Prerequisite = Tech String | Res String Int
-type Resource = { name:String, reqs:[Prerequisite] }
-type Technology = { name:String, cost:Price, upgrade:[Upgrade], reqs:[Prerequisite] }
+type Resource = { name:String, reqs:[Prerequisite], upkeep:[Price] }
+type Technology = { name:String, cost:Price, upgrade:[Upgrade], reqs:[Prerequisite], repeatable:Reps }
 
 techTable = D.fromList <| map (\t -> (t.name, t)) technologies
-technologies = let tech (name, cost, upgrade, reqs) = 
-                       {name=name, cost=cost, upgrade=upgrade, reqs=reqs}
+technologies = let tech (name, cost, upgrade, reqs, reps) = 
+                       {name=name, cost=cost, upgrade=upgrade, reqs=reqs, repeatable=reps}
                in map tech
-                      [ ("Clay Pit", [("Wood", 30)], [Income "Clay" 1], [])
-                      , ("Pottery", [("Wood", 25), ("Clay", 25)], [Capacity "Food" 10], [Tech "Clay Pit"])
-                      , ("Lumber Yard", [("Wood", 50), ("Stone", 10)], [Income "Wood" 1, Capacity "Wood" 10], [])
-                      , ("Quarry", [("Wood", 20), ("Stone", 50)], [Income "Stone" 1], [Tech "Lumber Yard"])
-                      , ("Farm", [("Wood", 30), ("Stone", 30)], [Income "Food" 1], [])
-                      , ("Mine", [("Wood", 50), ("Stone", 20), ("Food", 30)], [Income "Ore" 1, Income "Salt" 1], [Tech "Lumber Yard", Tech "Quarry", Tech "Farm"])
-                      , ("Gold Mine", [("Wood", 50), ("Stone", 20), ("Food", 30)], [Income "Gold" 1], [Tech "Mine"])
-                      , ("Blacksmith", [("Wood", 60), ("Stone", 50)], [], [Tech "Mine"])
-                      ]
+                      [ ("Clay Pit", [("Wood", 30)], [Income "Clay" 1], [], Inf)
+                      , ("Lumber Yard", [("Wood", 50), ("Stone", 10)], [Income "Wood" 1, Capacity "Wood" 10], [], Inf)
+                      , ("Farm", [("Wood", 30), ("Stone", 30)], [Income "Food" 1], [], Inf)
+                      , ("Pottery", [("Wood", 25), ("Clay", 25)], [Capacity "Food" 10], [Tech "Clay Pit"], Inf)
+                      , ("Quarry", [("Wood", 20), ("Stone", 50)], [Income "Stone" 1], [Tech "Lumber Yard"], Inf)
+                      , ("Mine", [("Wood", 50), ("Stone", 20), ("Food", 30)], [Income "Ore" 1, Income "Salt" 1], [Tech "Lumber Yard", Tech "Quarry", Tech "Farm"], Inf)
+                      , ("Gold Mine", [("Wood", 50), ("Stone", 20), ("Food", 30)], [Income "Gold" 1], [Tech "Mine"], Inf)
+                      , ("Blacksmith", [("Wood", 60), ("Stone", 50)], [], [Tech "Mine"], Inf)
+                      , ("Language", [], [Capacity "Workers" 100], [Res "Workers" 10], One)]
 
 resTable = D.fromList <| map (\r -> (r.name, r)) resources
-resources = let res (name, reqs) = {name=name, reqs=reqs}
-            in map res
-                   [ ("Wood", []), ("Stone", []),  ("Food", []), ("Clay", []), ("Workers", [Tech "Dwelling"])
-                   , ("Ore", [Tech "Mine"]), ("Salt", [Tech "Mine"]), ("Gold", [Tech "Gold Mine"])
-                   , ("Money", [Tech "Printing Press", Tech "Mint"]), ("Reputation", [Tech "Printing Press"])
-                   , ("Trade", [Tech "Shipyard"]), ("Credit", [Tech "Banking"]), ("Knowledge", [Tech "Philosophy"])
-                   ]
+resources = let base name = {name=name, reqs=[], upkeep=[]}
+                reqd (name, reqs) = {name=name, reqs=reqs, upkeep=[]}
+                uppd (name, reqs, upkeep) = {name=name, reqs=reqs, upkeep=upkeep}
+            in concat [ map base ["Wood", "Stone", "Food", "Clay"]
+                      , map reqd [ ("Workers", [Tech "Dwelling"])
+                                 , ("Ore", [Tech "Mine"]), ("Salt", [Tech "Mine"]), ("Gold", [Tech "Gold Mine"])
+                                 , ("Money", [Tech "Printing Press", Tech "Mint"]), ("Reputation", [Tech "Printing Press"])
+                                 , ("Trade", [Tech "Shipyard"]), ("Credit", [Tech "Banking"]), ("Knowledge", [Tech "Philosophy", Tech "Speech"])]
+                      , map uppd []]
 
 type GameState = { skill: D.Dict String Int
                  , income: D.Dict String Int
                  , workers: D.Dict String Int
                  , capacity: D.Dict String Int
-                 , built: S.Set String
-                 , balance: D.Dict String Int
-                 }
+                 , built: D.Dict String Int
+                 , balance: D.Dict String Int }
 
 defaultGame : GameState
 defaultGame = 
@@ -62,20 +62,37 @@ defaultGame =
     , income = D.empty
     , workers = D.empty
     , capacity = D.fromList [("Wood", 50), ("Stone", 100), ("Food", 50), ("Salt", 50), ("Ore", 50), ("Gold", 50), ("Clay", 50)]
-    , built = S.empty
-    , balance = D.fromList [("Workers", 2), ("Food", 10)]
-    }
+    , built = D.empty
+    , balance = D.fromList [("Workers", 2), ("Food", 10)]}
 
+resCap : GameState -> String -> Int
 resCap g res = find0 res g.capacity
 
+resTotal : GameState -> String -> Int
+resTotal g res = let balance = find0 res g.balance
+                 in case res of 
+                      "Workers" -> balance + (sum <| D.values g.workers)
+                      _         -> balance
+
+techBuilt : GameState -> String -> Int
+techBuilt g tech = find0 tech g.built
+
+incBalance : GameState -> [(String, Int)] -> GameState
 incBalance g amounts = let inc (res, amt) g = { g | balance <- updateWithDefault 0 res (\n -> min (resCap g res) (n + amt)) g.balance }
                        in foldr inc g amounts
 
 meetsPrereqs : GameState -> [Prerequisite] -> Bool
 meetsPrereqs g reqs = let meets prereq = case prereq of
-                                           Tech name -> S.member name g.built
+                                           Tech name -> (0 < techBuilt g name)
                                            Res name amt -> amt < find0 name g.balance
                       in and <| map meets reqs
+
+haventMaxed : GameState -> Technology -> Bool
+haventMaxed g tech = let built = techBuilt g tech.name
+                     in case tech.repeatable of
+                          Inf -> True
+                          One -> 1 > built
+                          Count num -> num > built
 
 canAfford : GameState -> Price -> Bool
 canAfford g cost = and <| map (\(res, amt) -> amt <= find0 res g.balance) cost
@@ -91,11 +108,12 @@ gather g res = let increment = sum [1, find0 res g.skill, find0 res g.workers]
 build : GameState -> String -> GameState
 build g name = case D.lookup name techTable of
                  Nothing -> g
-                 Just t -> case and [canAfford g t.cost, meetsPrereqs g t.reqs] of
-                             True -> applyUpgrades { g | balance <- decDict g.balance t.cost
-                                                   , built <- S.insert t.name g.built
-                                                   } t.upgrade
-                             _ -> g
+                 Just t -> case and [canAfford g t.cost, meetsPrereqs g t.reqs, haventMaxed g t] of
+                             True  -> applyUpgrades { g | balance <- decDict g.balance t.cost
+                                                        , built <- incDict g.built [(t.name, 1)]
+                                                    } t.upgrade
+                             False -> g
+                 _ -> g
 
 assign : GameState -> String -> GameState
 assign g name = case 0 < find0 "Workers" g.balance of
@@ -171,7 +189,7 @@ showGame g = flow down [ flow right <| resourceButtons g
                        , asText <| D.toList g.workers
                        , asText <| D.toList g.income
                        , asText <| D.toList g.capacity
-                       , asText <| S.toList g.built
+                       , asText <| D.toList g.built
                        , flow right <| techButtons g ]
 
 main = lift showGame game
@@ -183,6 +201,7 @@ printEvent ev = case ev of
                   Assign n -> "ASSIGNING TO " ++ n
                   Free n -> "FREEING FROM " ++ n
                   Tick -> "TICK"
+                  _ -> "The fuck? " ++ (show ev)
 
 port log : Signal String
 port log = lift printEvent events
