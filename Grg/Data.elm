feecaa7ac92c161199a0ade0701ad202513ea0fd
inaimathi <inaimathi@self>
2014-05-04 22:40:41 -0400

    -More fleshing out of data (still not quite there)

--- Grg/Data.elm
+++ Grg/Data.elm
@@ -19,96 +19,99 @@ type GameState = { skill: D.Dict String Int
                  , balance: D.Dict String Int }
 
 techTable = D.fromList <| map (\t -> (t.name, t)) technologies
-technologies = let tech (name, cost, upgrade, reqs, reps) = 
+technologies = let tech (name, upgrade, reqs) = 
+                       {name=name, cost=[], upgrade=upgrade, reqs=reqs, limit=One}
+                   struct (name, cost, upgrade, reqs, reps) =
                        {name=name, cost=cost, upgrade=upgrade, reqs=reqs, limit=reps}
-               in map tech
-                      [ ("Basic Stone Tools", [], [Income "Food" 5], [Res "Stone" 5], One)
-                      , ("Fire", [], [Income "Food" 5], [Res "Stone" 5, Res "Wood" 10], One)
-                      , ("Stone Tools", [], [Income "Food" 5], [Res "Stone" 15], One)
-                      , ("Language", [], [Capacity "Workers" 100], [Res "Workers" 15], One)
-                      , ("Ceramics", [], [Capacity "Food" 50], [Res "Clay" 25], One)
-                      , ("Domestication", [], [Capacity "Food" 50], [Res "Food" 100], One)
-                      , ("Hunting Implements", [], [Income "Food" 5, Capacity "Food" 50], [Res "Wood" 50], One)
-                      , ("Copper Tools", [], [Income "Food" 5, Capacity "Food" 10], [Tech "Copper Mine", Tech "Fire"], One)
-                      , ("Agriculture", [], [Income "Food" 10, Capacity "Food" 50], [Res "Wood" 50, Tech "Domestication", Res "Food" 50], One)
---                      , ("The Wheel", [], [], [], One)
-                      , ("Writing", [], [Income "Knowledge" 5, Capacity "Knowledge" 500], [Res "Workers" 200], One)
-                      , ("Bronze Tools", [], [Income "Food" 5, Capacity "Food" 10], [Tech "Bronze Tools", Tech "Bronze Mine"], One)
-                      , ("Salt", [], [Capacity "Food" 10], [Tech "Bronze Tools"], One)
-
-                      -- , ("Sundial", [], [], [], One)
-                      -- , ("Glass", [], [], [], One)
-                      -- , ("Optics", [], [], [], One)
-                      -- , ("Philosophy", [], [], [], One)
-                      -- , ("Mathematics", [], [], [], One)
-                      -- , ("Construction", [], [], [], One)
-                      -- , ("Iron Tools", [], [], [], One)
-
-                      -- , ("Engineering", [], [], [], One)
-                      -- , ("Metal Casting", [], [], [], One)
-                      -- , ("Education", [], [], [], One)
-                      -- , ("Machinery", [], [], [], One)
-                      -- , ("Physics", [], [], [], One)
-                      -- , ("Steel", [], [], [], One)
-
-                      -- , ("Astronomy", [], [], [], One)
-                      -- , ("Acoustics", [], [], [], One)
-                      -- , ("Printing Press", [], [], [], One)
-                      -- , ("Banking", [], [], [], One)
-
-                      -- , ("Navigation", [], [], [], One)
-                      -- , ("Chemistry", [], [], [], One)
-                      -- , ("Metallurgy", [], [], [], One)
-                      -- , ("Scientific Theory", [], [], [], One)
-                      -- , ("Fertilizer", [], [], [], One)
-                      -- , ("Biology", [], [], [], One)
-                      -- , ("Steam Power", [], [], [], One)
-
-                      -- , ("Electricity", [], [], [], One)
-                      -- , ("Railroad", [], [], [], One)
-
-                      -- , ("Refrigiration", [], [], [], One)
-                      -- , ("Telegraph", [], [], [], One)
-                      -- , ("Radio", [], [], [], One)
-                      -- , ("Flight", [], [], [], One)
-                      -- , ("Combustion", [], [], [], One)
-
-                      -- , ("Penicilin", [], [], [], One)
-                      -- , ("Plastics", [], [], [], One)
-                      -- , ("Electronics", [], [], [], One)
-                      -- , ("Mass Media", [], [], [], One)
-
-                      -- , ("Radar", [], [], [], One)
-                      -- , ("Atomic Theory", [], [], [], One)
-                      -- , ("Ecology", [], [], [], One)
-
-                      -- , ("Digital Computers", [], [], [], One)
-                      -- , ("Rocketry", [], [], [], One)
-                      -- , ("Lasers", [], [], [], One)
-                      -- , ("Nuclear Fission", [], [], [], One)
-                      -- , ("Robotics", [], [], [], One)
-                      -- , ("Satellites", [], [], [], One)
-
-                      -- , ("Particle Physics", [], [], [], One)
-                      -- , ("Nuclear Fusion", [], [], [], One)
-                      -- , ("Nanotechnology", [], [], [], One)
-                      -- , ("P=NP", [], [], [], One)
-                      -- , ("Artificial Intelligence", [], [], [], One)
-                      -- , ("Mind Uploading", [], [], [], One)                        
-                      -- , ("The Singularity", [], [], [], One)
-
-
---                      , ("Clay Pit", [("Wood", 30)], [Income "Clay" 3], [], Infinity)
---                      , ("Lumber Yard", [("Wood", 50), ("Stone", 10)], [Income "Wood" 3, Capacity "Wood" 10], [], Infinity)
---                      , ("Farm", [("Wood", 30), ("Stone", 30)], [Income "Food" 3, Capacity "Workers" 5], [], Infinity)
---                      , ("Granary", [("Wood", 40), ("Stone", 30)], [Capacity "Food" 15], [Tech "Farm"], Infinity)
---                      , ("Dwelling", [("Wood", 20), ("Stone", 20)], [Capacity "Workers" 2], [Tech "Farm"], Infinity)
---                      , ("Pottery", [("Wood", 25), ("Clay", 25)], [Capacity "Food" 10], [Tech "Clay Pit"], Infinity)
---                      , ("Quarry", [("Wood", 20), ("Stone", 50)], [Income "Stone" 3], [Tech "Lumber Yard"], Infinity)
---                      , ("Mine", [("Wood", 50), ("Stone", 20), ("Food", 30)], [Income "Ore" 1, Income "Salt" 1], [Tech "Lumber Yard", Tech "Quarry", Tech "Farm"], Infinity)
---                      , ("Gold Mine", [("Wood", 50), ("Stone", 20), ("Food", 30)], [Income "Gold" 1], [Tech "Mine"], Infinity)
---                      , ("Blacksmith", [("Wood", 60), ("Stone", 50)], [Income "Iron" 1], [Tech "Mine"], Infinity) -- Upgrade here
-                      ]
+               in concat 
+                  [map tech [ ("Basic Stone Tools", [Income "Food" 5], [Res "Stone" 5])
+                            , ("Fire", [Income "Food" 5], [Res "Stone" 5, Res "Wood" 10])
+                            , ("Stone Tools", [Income "Food" 5], [Res "Stone" 15, Tech "Basic Stone Tools"])
+                            , ("Language", [Capacity "Workers" 100], [Res "Workers" 25])
+                            , ("Ceramics", [Capacity "Food" 50], [Res "Clay" 25])
+                            , ("Domestication", [Capacity "Food" 50], [Res "Food" 100])
+                            , ("Hunting Implements", [Income "Food" 5, Capacity "Food" 50], [Res "Wood" 50])
+                            , ("Copper Tools", [Income "Food" 5, Capacity "Food" 10], [Tech "Copper Mine", Tech "Fire", Tech "Stone Tools"])
+                            , ("Agriculture", [Income "Food" 10, Capacity "Food" 50], [Res "Wood" 50, Tech "Domestication", Res "Food" 50])
+                            , ("The Wheel", [Income "Food" 10, Capacity "Workers" 10], [Res "Wood" 50, Tech "Agriculture", Res "Knowledge" 50])
+                            , ("Writing", [Income "Knowledge" 5, Capacity "Knowledge" 500], [Res "Workers" 200])
+                            , ("Bronze Tools", [Income "Food" 5, Capacity "Food" 10], [Tech "Fire", Tech "Bronze Mine"])
+                            , ("Salt", [Capacity "Food" 10], [Tech "Bronze Tools"])
+                              
+                            , ("Sundial", [], [Res "Knowledge" 250, Tech "Stone Tools"])
+                            , ("Glass", [], [Res "Knowledge" 250, Tech "Fire"])
+                            , ("Optics", [], [Res "Knowledge" 500, Tech "Glass"])
+                            , ("Philosophy", [Income "Knowledge" 10, Capacity "Knowledge" 500], [Tech "Writing", Res "Knowledge" 500, Res "Workers" 250])
+                            , ("Mathematics", [], [Tech "Philosophy", Res "Knowledge" 500])
+                            , ("Construction", [], [Tech "Mathematics", Tech "Bronze Tools"])
+                            , ("Iron Tools", [], [Tech "Bronze Tools"])
+                              
+                            , ("Engineering", [], [Tech "Mathematics", Tech "Construction"])
+                            , ("Metal Casting", [], [Tech "Iron Tools"])
+                            , ("Education", [Income "Knowledge" 10, Capacity "Knowledge" 500], [Tech "Mathematics", Tech "Philosophy", Res "Knowledge" 750])
+                            , ("Machinery", [], [Tech "Engineering"])
+                            , ("Physics", [], [Tech "Mathematics"])
+                            , ("Steel", [], [Tech "Engineering", Tech "Metal Casting"])
+                              
+                            , ("Astronomy", [], [Tech "Optics", Tech "Engineering", Tech "Physics"])
+                            , ("Acoustics", [], [Tech "Engineering", Tech "Machinery", Tech "Physics"])
+                            , ("Printing Press", [Income "Knowledge" 10, Capacity "Knowledge" 500], [Tech "Engineering"])
+                            , ("Banking", [], [Tech "Printing Press"])
+                              
+                            -- , ("Navigation", [], [])
+                            -- , ("Chemistry", [], [])
+                            -- , ("Metallurgy", [], [])
+                            -- , ("Scientific Theory", [], [])
+                            -- , ("Fertilizer", [], [])
+                            -- , ("Biology", [], [])
+                            -- , ("Steam Power", [], [])
+                              
+                            -- , ("Electricity", [], [])
+                            -- , ("Railroad", [], [])
+                              
+                            -- , ("Refrigiration", [], [])
+                            -- , ("Telegraph", [], [])
+                            -- , ("Radio", [], [])
+                            -- , ("Flight", [], [])
+                            -- , ("Combustion", [], [])
+                              
+                            -- , ("Penicilin", [], [])
+                            -- , ("Plastics", [], [])
+                            -- , ("Electronics", [], [])
+                            -- , ("Mass Media", [], [])
+                              
+                            -- , ("Radar", [], [])
+                            -- , ("Atomic Theory", [], [])
+                            -- , ("Ecology", [], [])
+                              
+                            -- , ("Digital Computers", [], [])
+                            -- , ("Rocketry", [], [])
+                            -- , ("Lasers", [], [])
+                            -- , ("Nuclear Fission", [], [])
+                            -- , ("Robotics", [], [])
+                            -- , ("Satellites", [], [])
+                              
+                            -- , ("Particle Physics", [], [])
+                            -- , ("Nuclear Fusion", [], [])
+                            -- , ("Nanotechnology", [], [])
+                            , ("P=NP", [], [Tech "Mathematics", Tech "Artificial Intelligence", Res "Knowledge" 25000])
+                            , ("Artificial Intelligence", [], [Tech "Digital Computers"])
+                            , ("Mind Uploading", [], [Tech "Nanotechnology", Tech "Digital Computers", Tech "Biology"])
+                            , ("The Singularity", [], [Tech "P=NP", Tech "Artificial Intelligence", Tech "Mind Uploading"])
+                            ],
+                   
+                  map struct [ ("Clay Pit", [("Wood", 30)], [Income "Clay" 3], [], Infinity)
+                             , ("Lumber Yard", [("Wood", 50), ("Stone", 10)], [Income "Wood" 3, Capacity "Wood" 10], [], Infinity)
+                             , ("Farm", [("Wood", 30), ("Stone", 30)], [Income "Food" 3, Capacity "Workers" 5], [], Infinity)
+                             , ("Granary", [("Wood", 40), ("Stone", 30)], [Capacity "Food" 15], [Tech "Farm"], Infinity)
+                             , ("Dwelling", [("Wood", 20), ("Stone", 20)], [Capacity "Workers" 2], [Tech "Farm"], Infinity)
+                             , ("Pottery", [("Wood", 25), ("Clay", 25)], [Capacity "Food" 10], [Tech "Clay Pit"], Infinity)
+                             , ("Quarry", [("Wood", 20), ("Stone", 50)], [Income "Stone" 3], [Tech "Lumber Yard"], Infinity)
+                             , ("Mine", [("Wood", 50), ("Stone", 20), ("Food", 30)], [Income "Ore" 1, Income "Salt" 1], [Tech "Lumber Yard", Tech "Quarry", Tech "Farm"], Infinity)
+                             , ("Gold Mine", [("Wood", 50), ("Stone", 20), ("Food", 30)], [Income "Gold" 1], [Tech "Mine"], Infinity)
+                             , ("Blacksmith", [("Wood", 60), ("Stone", 50)], [Income "Iron" 1], [Tech "Mine"], Infinity)
+                             ]]
+                      
 
 resTable = D.fromList <| map (\r -> (r.name, r)) resources
 resources = let base name = {name=name, reqs=[], upkeep=[]}
